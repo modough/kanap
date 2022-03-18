e42543b312765ea5d200d55b3fe6ab7b97595153
mouhamed <mouhamed.mbaye1@outlook.com>
2022-03-18 18:11:36 +0100

    product page colors chgt

--- front/js/product.js
+++ front/js/product.js
@@ -3,47 +3,45 @@ const fetchElement = async (Url) => {
     .then((res) => res.json())
     .then((data) => {
        displayElement(data);
+       console.log(data)
        
            
     })    
 };
 
 const displayElement = (elementData) => {
-   // for (let i = 0; i < elementData.length; i ++){
-        let i = 0; i < elementData.length; i ++;
+  
+    let i = 0; i < elementData.length; i ++;
+    let myElementId = document.createElement("a");
+    let myElementImage = document.createElement("img");
+    let myElementTitle = document.getElementById("title");
+    let myElementPrice = document.getElementById("price")
+    let myElementDescription = document.getElementById("description");
+    let myElementColor = document.createElement("option");
+   
+    
 
-        
-        
-        let myElementImage = document.createElement("img");
-        let myElementTitle = document.createElement("h1");
-        let myElementPrice = document.createElement("span")
-        let myElementDescription = document.createElement("p");
-        let myElementColor = document.createElement("option");
-        let myElementQuantity = document.createElement("input");
-
-        let myImgDiv = document.querySelector("div.item__img");
-        myImgDiv.appendChild(myElementImage);
-
-        let myTitleDiv = document.querySelector("div.item__content__titlePrice");
-        
-        let myPriceParagraph = document.querySelector("p");
-        myTitleDiv.appendChild(myElementTitle, myPriceParagraph);
-        myPriceParagraph.appendChild(myElementPrice);
-        let myDescriptionDiv = document.querySelector("div.item__content__description");
-        myDescriptionDiv.appendChild(myElementDescription);
-        
-        
-       
-        
-        
-        myElementImage.src = elementData[i].imageUrl;
-        myElementTitle.textContent = elementData[i].name;
-        myElementPrice.textContent = elementData[i].price;
-        myElementDescription.textContent = elementData[i].description;
-        myElementColor.textContent = elementData[i].colors;
-        
-
-        
-    //}
+    let myImgDiv = document.querySelector("div.item__img");
+    myImgDiv.appendChild(myElementImage);
+    let myTitleDiv = document.querySelector("div.item__content__titlePrice");
+    let myPriceParagraph = document.querySelector("p");
+    myTitleDiv.appendChild(myElementTitle, myPriceParagraph);
+    myPriceParagraph.appendChild(myElementPrice);
+    let myDescriptionDiv = document.querySelector("div.item__content__description");
+    myDescriptionDiv.appendChild(myElementDescription);
+    let colorSelect = document.getElementById("colors");
+    colorSelect.appendChild(myElementColor);
+    
+    
+    myElementImage.src = elementData[i].imageUrl;
+    myElementTitle.textContent = elementData[i].name;
+    myElementPrice.textContent = elementData[i].price;
+    myElementDescription.textContent = elementData[i].description;  
+    myElementColor.textContent = elementData[i].colors;  
+    myElementId.href = elementData[i]._id;
 };
+
+
+
+
 fetchElement("http://localhost:3000/api/products");
