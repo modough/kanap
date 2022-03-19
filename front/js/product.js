abbc1b8ed4689799d3735b6a3e631c2d6a62881f
mouhamed <mouhamed.mbaye1@outlook.com>
2022-03-19 16:54:55 +0100

    display product page ok

--- front/js/product.js
+++ front/js/product.js
@@ -1,47 +1,46 @@
-const fetchElement = async (Url) => {
-    await fetch(Url)
+const fetchElement = async () => {
+    await fetch(`http://localhost:3000/api/products/${productId}`)
     .then((res) => res.json())
     .then((data) => {
-       displayElement(data);
-       console.log(data)
-       
-           
+       displayElement(data);       
     })    
 };
+let productId = window.location.search.replace("?id=", "");
 
 const displayElement = (elementData) => {
-  
-    let i = 0; i < elementData.length; i ++;
-    let myElementId = document.createElement("a");
+    
     let myElementImage = document.createElement("img");
     let myElementTitle = document.getElementById("title");
     let myElementPrice = document.getElementById("price")
     let myElementDescription = document.getElementById("description");
     let myElementColor = document.createElement("option");
-   
     
-
-    let myImgDiv = document.querySelector("div.item__img");
+    
+    let myImgDiv = document.querySelector(".item__img");
     myImgDiv.appendChild(myElementImage);
-    let myTitleDiv = document.querySelector("div.item__content__titlePrice");
+    let myTitleDiv = document.querySelector(".item__content__titlePrice");
     let myPriceParagraph = document.querySelector("p");
     myTitleDiv.appendChild(myElementTitle, myPriceParagraph);
     myPriceParagraph.appendChild(myElementPrice);
-    let myDescriptionDiv = document.querySelector("div.item__content__description");
+    let myDescriptionDiv = document.querySelector(".item__content__description");
     myDescriptionDiv.appendChild(myElementDescription);
     let colorSelect = document.getElementById("colors");
     colorSelect.appendChild(myElementColor);
+    let myQuantityDiv = document.querySelector("item__content__settings__quantity");
+    
+
     
     
-    myElementImage.src = elementData[i].imageUrl;
-    myElementTitle.textContent = elementData[i].name;
-    myElementPrice.textContent = elementData[i].price;
-    myElementDescription.textContent = elementData[i].description;  
-    myElementColor.textContent = elementData[i].colors;  
-    myElementId.href = elementData[i]._id;
+    myElementImage.src = elementData.imageUrl;
+    myElementTitle.textContent = elementData.name;
+    myElementPrice.textContent = elementData.price;
+    myElementDescription.textContent = elementData.description;  
+    myElementColor.textContent = elementData.colors;  
+    
+    
 };
 
 
 
 
-fetchElement("http://localhost:3000/api/products");
+fetchElement();
