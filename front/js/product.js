154c02987c61066ce1c22ee678115e8c7bdaab78
mouhamed <mouhamed.mbaye1@outlook.com>
2022-03-24 18:42:18 +0100

    product.js restructured

--- front/js/product.js
+++ front/js/product.js
@@ -9,31 +9,33 @@ const fetchElement = async () => {
 let elementId = window.location.search.replace("?id=", "");
 
 const displayElement = (elementData) => {
-   
     let myElementImage = document.createElement("img");
     let myElementTitle = document.getElementById("title");
     let myElementPrice = document.getElementById("price")
     let myElementDescription = document.getElementById("description");
+    let myElementcolor = document.getElementById("colors");
     let myElementQuantity = document.getElementById("quantity");
+    
+    for(let i in elementData.colors){
+        myElementcolor.innerHTML += `<option>${elementData.colors[i]}</option>`;
+    };
+
     let myImgDiv = document.querySelector(".item__img");
     let myTitleDiv = document.querySelector(".item__content__titlePrice");
     let myDescriptionDiv = document.querySelector(".item__content__description");
-    let colorSelect = document.getElementById("colors");
-    for(let i in elementData.colors){
-        colorSelect.innerHTML += `<option>${elementData.colors[i]}</option>`;
-    };
-    let colorSelectDiv = document.querySelector(".item__content__settings__color");
-    let myColorAndQuantityDiv = document.querySelector(".item__content__settings");
+    let myColorDiv =document.querySelector(".item__content .item__content__settings .item__content__settings__color")
+    let myQuantityDiv = document.querySelector(".item__content .item__content__settings .item__content__settings__quantity");
 
     myImgDiv.appendChild(myElementImage);
     myTitleDiv.appendChild(myElementTitle, myElementPrice);
     myDescriptionDiv.appendChild(myElementDescription);
-    myColorAndQuantityDiv.appendChild(colorSelectDiv, myElementQuantity);
+    myColorDiv.appendChild(myElementcolor);
+    myQuantityDiv.appendChild(myElementQuantity);
+
     myElementImage.src = elementData.imageUrl;
     myElementTitle.textContent = elementData.name;
     myElementPrice.textContent = elementData.price;
-    myElementDescription.textContent = elementData.description;  
-       
+    myElementDescription.textContent = elementData.description;        
 };
 
 fetchElement();
